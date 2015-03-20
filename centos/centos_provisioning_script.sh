ce115617382d2eb3c30c9bacb87884e40e098713
pedro.galvao@gmail.com <ptg@localhost.localdomain>
2015-03-20 02:14:01 +0000

    added centos prov script

---
+++ centos/centos_provisioning_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Post-provisioning script for CENTOS
+# Simple actions to keep a machine with really basic security
+
+{
+echo "Setting up firewall rules..."
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT
+iptables -A INPUT -j LOG # Delete after entering production if it causes too much IO
+iptables -P INPUT DROP
+
+echo "Here are the fw rules..."
+iptables -L -vn --line
+
+echo "Saving rules..."
+/sbin/service iptables save
+
+echo "Performing system update..."
+yum -y update
+
+echo "Installing pySLAPI..."
+yum -y install python-pip
+pip install softlayer
+} | tee $0.log
