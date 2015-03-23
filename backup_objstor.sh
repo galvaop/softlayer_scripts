7d9e3829731392079a279f248714ea0c2c4de260
pedro.galvao@gmail.com <ptg@localhost.localdomain>
2015-03-23 13:53:36 +0000

    new files and changes reorg to sap_hana_rhel_prepare.sh

---
+++ backup_objstor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Backs up file to object storage via sftp
+
+
+export LOGNAME="<logon name>"
+export FTP_PASSWORD="<api key>"
+export TARGET_URL="sftp://lon02.objectstorage.service.networklayer.com/<container>/<directory>"
+export OPTIONS="--full-if-older-than 1M --encrypt-key <gpg key id>"
+export SOURCE_DIRS="--include <dir 1> --include <dir n>" 
+
+# Check what needs to be done if nothing defined then just backup
+case $1 in
+	status)
+		echo "Showing backup status..."
+		duplicity collection-status $TARGET_URL $OPTIONS
+		;;
+	restore)
+		echo "Restoring files..."
+		duplicity $TARGET_URL $2 $OPTIONS
+		;;
+		
+	backup)
+		echo "Backing up..."
+		duplicity $SOURCE_DIRS --exclude '**' / $TARGET_URL $OPTIONS
+		;;
+	*)
+		echo "Usage $0 <status |restore <restoredir> | backup | shell>."
+		;;
+esac
--- sap/sap_hana_rhel_prepare.sh
+++ sap/sap_hana_rhel_prepare.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Prep for SAP HANA ENV
-# Base on http://help.sap.com/hana/red_hat_enterprise_linux_rhel_6_5_configuration_guide_for_sap_hana_en.pdf
+# Based on http://help.sap.com/hana/red_hat_enterprise_linux_rhel_6_5_configuration_guide_for_sap_hana_en.pdf
 # Log all the work
 
 {
+# Firewalling
 echo "Setting up firewall rules..."
 iptables -F
 iptables -X
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i lo	-j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT
 iptables -A INPUT -j LOG
@@ -19,9 +21,9 @@ iptables -L -vn --line
 echo "Saving firewall rules for next reboot..."
 service iptables save
 
-# Assuming we're on 6.6
-echo "Downgrading redhat release to 6.5..."
-sudo yum downgrade redhat-release -y
+# NTP Configuration
+echo "Setting timezone to Lisbon..."
+ln -f /usr/share/zoneinfo/Europe/Lisbon /etc/localtime
 
 echo "Synchronizing clock..."
 service ntpd stop
@@ -36,15 +38,9 @@ echo "Setup ntpdate to adjust clock on reboot for larger drifts"
 echo servertime.service.softlayer.com >> /etc/ntp/step-tickers
 chkconfig ntpdate on
 
+# Version lock
 echo "Installing yum-version lock and security"
 yum -y install yum-versionlock yum-security
-
-echo "Checking versions..."
-yum --showduplicates nss-softokn # <= 3.14.3-12.el6_5
-yum --showduplicates nss-softokn-freebl # <= 3.14.3-12.el6_5
-yum --showduplicates kernel # <= 2.6.32-431.29.2.el6
-yum --showduplicates kernel-haders # <= 2.6.32-431.29.2.el6
-yum --showduplicates kernel-firmware # <= 2.6.32-431.29.2.el6
 
 echo "Creating versionlock file list..."
 cat > /etc/yum/pluginconf.d/versionlock.list <<EOT
@@ -56,8 +52,29 @@ nss-softokn-3.14.3-12.el6_5.*
 redhat-release-server-6Server-6.5.0.1.el6.*
 EOT
 
+# Assuming we're on 6.6 - downgrade to supported version
 echo "Performing security updates..."
 yum -y --security update
+
+echo "Downgrading redhat release to 6.5..."
+sudo yum downgrade redhat-release -y
+
+echo "Checking versions and attempting downgrade..."
+yum --showduplicates install nss-softokn-3.14.3-12.el6_5
+yum --showduplicates install nss-softokn-freebl-3.14.3-12.el6_5
+yum --showduplicates install kernel-2.6.32-431.29.2.el6
+yum --showduplicates install kernel-headers-2.6.32-431.29.2.el6
+yum --showduplicates install kernel-firmware-2.6.32-431.29.2.el6
+
+echo "Updating kernel. Should not do anything if the downgrade worked..."
+yum -y update kernel kernel-firmware
+
+echo "Installing kernel headers. Same as above..."
+yum -y install kernel-headers.x86_64
+
+# Package install
+echo "Adding EPEL repo..."
+rpm -Uvh http://ftp.uni-kl.de/pub/linux/fedora-epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 echo "Installing base + xfsprogs..."
 yum -y groupinstall base
@@ -66,29 +83,8 @@ yum -y install xfsprogs
 echo "Installing x11-auth..."
 yum install xorg-x11-xauth -y
 
-echo "Updating kernel..."
-yum -y update kernel kernel-firmware
-
-echo "Installing kernel headers..."
-yum -y install kernel-headers.x86_64
-
-echo "Adding EPEL repo..."
-rpm -Uvh http://ftp.uni-kl.de/pub/linux/fedora-epel/6/x86_64/epel-release-6-8.noarch.rpm
-
 echo "Installing iperf..."
 yum -y install iperf
-
-echo "Installing duplicity for backups..."
-yum -y install duplicity.x86_64 
-
-echo "Downloading compat-sap++"
-wget http://ftp.redhat.com/pub/redhat/linux/enterprise/6Server/en/os/SRPMS/compat-sap-c++-4.7.2-10.el6_5.src.rpm
-
-echo "Installing rpm-build"
-yum -y install rpm-build.x86_64
-
-echo "Installing development tools"
-yum -y groupinstall "development tools"
 
 echo "Installing numactl..."
 yum -y install numactl.x86_64
@@ -96,14 +92,38 @@ yum -y install numactl.x86_64
 echo "Installing java..."
 yum -y install icedtea-web
 
+echo "Installing other required packages..."
+yum install gtk2 libicu xulrunner ntp sudo tcsh libssh2 expect cairo graphviz iptraf krb5-workstation krb5-libs.i686 krb5-libs nfs-utils lm_sensors rsyslog openssl098e openssl xauth PackageKit-gtk-module libcanberra-gtk2 libtool-ltdl gcc glib glib-devel glibc libc-devel zlib-devel libstdc++-devel kernel-devel rpm-build redhat-rpm-config numactl iperf compat-sap-c++
+
+echo "Installing duplicity for backups..."
+yum -y install duplicity.x86_64 
+
+echo "Downloading compat-sap++"
+wget http://ftp.redhat.com/pub/redhat/linux/enterprise/6Server/en/os/SRPMS/compat-sap-c++-4.7.2-10.el6_5.src.rpm
+
+echo "Installing development tools"
+yum -y groupinstall "development tools"
+
+
+# More configuration
+echo "Stopping and disabling kdump..."
+service kdump stop
+chkconfig kdump off
+
+echo "Stopping and disabling abrt related stuff..."
+service abrtd stop
+service abrt-ccpp stop
+chkconfig abrtd off
+chkconfig abrt-ccpp off
+
 echo "Setting up symbolic links..."
 ln -s /usr/lib64/libssl.so.0.9.8e /usr/lib64/libssl.so.0.9.8
 ln -s /usr/lib64/libssl.so.1.0.1e /usr/lib64/libssl.so.1.0.1
 ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.0.9.8
 ln -s /usr/lib64/libcrypto.so.1.0.1e /usr/lib64/libcrypto.so.1.0.1
 
-echo "Disabling kernel huge pages..."
-sed -i 's/biosdevname=0/biosdevname=0 transparent_hugepage=never/g' /boot/grub/grub.conf
+echo "Disabling kernel huge pages and setting cpu idle states..."
+sed -i 's/biosdevname=0/biosdevname=0 transparent_hugepage=never intel_idle.max_cstate=0 processor.max_cstate=0/g' /boot/grub/grub.conf
 
 echo "Setting up tuned profile..."
 yum -y install tuned
@@ -120,16 +140,6 @@ EOT
 echo "Creating directories for hana mountpoints..."
 mkdir -p /hana/{shared,data,log}
 mkdir -p /usr/sap
-
-echo "Stopping and disabling kdump..."
-service kdump stop
-chkconfig kdump off
-
-echo "Stopping and disabling abrt related stuff..."
-service abrtd stop
-service abrt-ccpp stop
-chkconfig abrtd off
-chkconfig abrt-ccpp off
 
 echo "Cleaning up old kernels..."
 package-cleanup --oldkernels --count=1 -y
