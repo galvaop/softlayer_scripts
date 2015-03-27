f4f9f09802e458d4a837244894c2cd5fa5c78ea4
pedro.galvao@gmail.com <ptg@localhost.localdomain>
2015-03-27 14:56:18 +0000

    updated backup_objectstor.sh

--- backup_objstor.sh
+++ backup_objstor.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-# Backs up file to object storage via sftp
 
-export LOGNAME="<logon name>"
-export FTP_PASSWORD="<api key>"
-export TARGET_URL="sftp://lon02.objectstorage.service.networklayer.com/<container>/<directory>"
-export OPTIONS="--full-if-older-than 1M --encrypt-key <gpg key id>"
-export SOURCE_DIRS="--include <dir 1> --include <dir n>" 
+export HOME="/root"
+export LOGNAME=""
+export FTP_PASSWORD=""
+export TARGET_URL=""
+export OPTIONS="--full-if-older-than 3M --encrypt-key <keyid> --progress"
+export SOURCE_DIRS="--include xxx --include yyy" 
 
-# Check what needs to be done if nothing defined then just backup
+# Check what needs to be done if nothing defined then inform
 case $1 in
 	status)
 		echo "Showing backup status..."
-		duplicity collection-status $TARGET_URL $OPTIONS
+		time duplicity collection-status $TARGET_URL $OPTIONS
 		;;
 	restore)
 		echo "Restoring files..."
-		duplicity $TARGET_URL $2 $OPTIONS
+		time duplicity $TARGET_URL $2 $OPTIONS
 		;;
-		
+	list)
+		echo "Listing files..."
+		time duplicity list-current-files $TARGET_URL $OPTIONS
+		;;		
 	backup)
 		echo "Backing up..."
-		duplicity $SOURCE_DIRS --exclude '**' / $TARGET_URL $OPTIONS
+		time duplicity $SOURCE_DIRS --exclude '**' / $TARGET_URL $OPTIONS
 		;;
 	*)
-		echo "Usage $0 <status |restore <restoredir> | backup | shell>."
+		echo "Usage $0 <status |restore <restoredir> | backup | list>."
 		;;
 esac
