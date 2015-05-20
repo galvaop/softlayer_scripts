7e2e3d5cc2f094817531d2505c9ea3860a56abdf
ptg <ptg@localhost.localdomain>
2015-05-20 17:26:27 +0100

    cleanup

--- sl_provision_vs.sh
+++ sl_provision_vs.sh
@@ -11,19 +11,16 @@ RAM="1"
 OS="ubuntu_latest"
 BILLING="hourly"
 NETWORK="100"
-SSHKEY="<sshkey>"
-POSTINSTALL="<postinstall script URL>"
+SSHKEY="<KEY>"
+POSTINSTALL="<provisioning script>"
 DISK="25"
 OTHER_OPTIONS=""
 
 echo "Using SL account:"
 slcli config show
 
-echo "Estimated per machine cost:"
-#slcli vs create --hostname $HOSTNAME --domain $DOMAIN --cpu $CPU --memory $RAM $OPTIONS --test
-# slcli vs create --hostname $HOSTNAME --domain $DOMAIN --cpu $CPU --memory $RAM $OPTIONS
-
 # Get a quote
+echo "Estimated per machine cost:"
 slcli vs create --domain $DOMAIN --hostname $HOSTNAME --cpu $CPU --memory $RAM --os $OS --datacenter $DATACENTER --billing $BILLING --network $NETWORK --disk $DISK --postinstall $POSTINSTALL --key $SSHKEY $OTHER_OPTIONS --test
 
 # Create after confirm
